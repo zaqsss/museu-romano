0e53df41f3490819e85408f7feee19f2327bc714
zaqsss <isaquecordeiro9855@gmail.com>
2024-09-25 15:14:43 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function toggleDescription(descId) {
+    const description = document.getElementById(descId);
+    description.style.display = description.style.display === 'none' ? 'block' : 'none';
+}
